a79dbc4764b3dc3f435abf2424ec403d2490994a
reza72rg <reza72rg@gmail.com>
2024-07-26 20:42:35 +0330

    I change models.

--- Feedbacks/admin.py
+++ Feedbacks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Feedbacks.models import Question, Answer
 # Register your models here.
+
+
+admin.site.register(Question)
+admin.site.register(Answer)
+
--- Feedbacks/models.py
+++ Feedbacks/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
+from Forms.models import Form
 from django.db import models
-
 # Create your models here.
+
+
+class Question(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    question_type = models.CharField(max_length=255)
+    body = models.TextField()
+    page = models.IntegerField(default=0)
+    list_of_select = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return self.body
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return f'Answer to {self.question.body} by {self.user.username}'
