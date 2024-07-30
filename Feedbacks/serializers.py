449ed4c350058d928b055cc552109d81999d637f
farbod <farbod.rezabakhsh81@gmail.com>
2024-07-30 17:23:56 +0330

    Chnage feedbacks/serializers to show the answers.

--- Feedbacks/serializers.py
+++ Feedbacks/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import Answer,Question
 
 class QuestionSerializer(serializers.ModelSerializer):
+    answers = serializers.SerializerMethodField()
     class Meta:
         model = Question
         fields = '__all__'
+
+    def get_answers(self,obj):
+        result = obj.answers.all()
+        return AnswerSerializer(instance=result,many=True).data
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
