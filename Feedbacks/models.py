34fcf0fe58a87f96552d501c27c7f1519a65c4b4
farbod <farbod.rezabakhsh81@gmail.com>
2024-07-30 17:11:54 +0330

    Creating views and make some changes in feedbacks/models.py

--- Feedbacks/models.py
+++ Feedbacks/models.py
@@ -5,20 +5,32 @@ from django.db import models
 
 
 class Question(models.Model):
+    CHOICES = [
+        ('survey','Survey'),
+        ('quiz','Quiz'),
+        ('poll','Poll'),
+    ]
     form = models.ForeignKey(Form, on_delete=models.CASCADE)
-    question_type = models.CharField(max_length=255)
+    question_type = models.CharField(max_length=255,choices=CHOICES)
+    title = models.CharField(max_length=100)
     body = models.TextField()
-    page = models.IntegerField(default=0)
-    list_of_select = models.JSONField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.body
+
+    def get_question_by_form_type(form_type):
+        return Question.objects.filter(form__form_type=form_type)
 
 
 class Answer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Answer to {self.question.body} by {self.user.username}'
+        return f'Answer to {self.question.title} by {self.user.username}'
+
+    def get_answers_by_question_type(form_type):
+        return Answer.objects.filter(question__form__form_type=form_type)
